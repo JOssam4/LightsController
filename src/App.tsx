1abcc46ea2ee823019c00ef407432e57905807e3
james <jamesossam@gmail.com>
2022-11-23 17:20:23 -0800

    Add device picker

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { ColorPicker, useColor, toColor, Color} from 'react-color-palette';
 import "react-color-palette/lib/css/styles.css";
 import './App.css';
+import DevicePicker, { Device } from './DevicePicker';
 
 interface ColorResponse {
   h: number;
@@ -11,11 +12,13 @@ interface ColorResponse {
 
 interface State {
   initialQueryCompleted: boolean;
+  currentDevice: Device;
 }
 
 function App() {
   const [state, setState] = useState<State>({
     initialQueryCompleted: false,
+    currentDevice: Device.COMPUTER,
   });
   const [color, setColor] = useColor("hex", "#121212");
 
@@ -23,7 +26,7 @@ function App() {
     const hsv = color.hsv;
     console.log(`sending request with hsv:`);
     console.dir(hsv);
-    fetch('http://localhost:3001/color', {
+    fetch(`http://localhost:3001/color?device=${state.currentDevice}`, {
       method: 'PUT',
       headers: {
         'Accept': 'application/json',
@@ -34,19 +37,26 @@ function App() {
   }
 
   useEffect(() => {
-    fetch('http://localhost:3001/color')
+    fetch(`http://localhost:3001/color?device=${state.currentDevice}`)
       .then((resp) => resp.json())
       .then((currentColor: ColorResponse) => {
         const {h, s, v} = currentColor;
-        setState({ initialQueryCompleted: true });
+        setState({ ...state, initialQueryCompleted: true });
         setColor(toColor('hsv', {h, s, v}));
       });
   }, []);
+
+  function setCurrentDevice(newDevice: Device) {
+    setState({ ...state, currentDevice: newDevice });
+  }
 
 
   if (state.initialQueryCompleted) {
     return (
       <div className="App">
+          <div>
+            <DevicePicker setCurrentDevice={(currentDevice: Device) => setCurrentDevice(currentDevice)} currentDevice={state.currentDevice} />
+          </div>
           <div>
             <ColorPicker width={456} color={color} onChange={setColor} onChangeComplete={(color) => updateLights(color)} dark />
           </div>
---
+++ src/DevicePicker.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faDesktop, faBed } from "@fortawesome/free-solid-svg-icons";
+
+enum Device {
+  BEDROOM,
+  COMPUTER,
+}
+
+interface Props {
+  currentDevice: Device;
+  setCurrentDevice: (device: Device) => void;
+}
+
+interface State {
+  currentDevice: Device;
+}
+
+export default function DevicePicker(props: Props) {
+  const [state, setState] = useState<State>({
+    currentDevice: props.currentDevice,
+  });
+
+  function handleDeviceChange(event: React.MouseEvent, newDevice: Device) {
+    console.log(`newDevice: ${newDevice}`);
+    props.setCurrentDevice(newDevice);
+    setState({ ...state, currentDevice: newDevice });
+  }
+
+  return (
+    <ToggleButtonGroup value={state.currentDevice} exclusive onChange={(event, newDevice: Device) => handleDeviceChange(event, newDevice)} aria-label="device chooser">
+      <ToggleButton value={Device.BEDROOM} aria-label="bedroom lights">
+        <FontAwesomeIcon icon={faBed} />
+      </ToggleButton>
+      <ToggleButton value={Device.COMPUTER} aria-label="computer lights">
+        <FontAwesomeIcon icon={faDesktop} />
+      </ToggleButton>
+    </ToggleButtonGroup>
+  )
+}
+
+export { Device };
