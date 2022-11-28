2ba85fc344b99387b014350c39991a5b7ac68266
james <jamesossam@gmail.com>
2022-11-27 16:20:44 -0800

    Compartmentalized picker

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { ColorPicker, useColor, toColor, Color} from 'react-color-palette';
 import "react-color-palette/lib/css/styles.css";
 import './App.css';
 import DevicePicker, { Device } from './DevicePicker';
+import Picker from './Picker';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSpinner} from '@fortawesome/free-solid-svg-icons';
 
 interface ColorResponse {
   h: number;
@@ -11,61 +14,69 @@ interface ColorResponse {
 }
 
 interface State {
-  initialQueryCompleted: boolean;
   currentDevice: Device;
+  livingroomColor: Color | null;
+  bedroomColor: Color | null;
 }
 
 function App() {
   const [state, setState] = useState<State>({
-    initialQueryCompleted: false,
     currentDevice: Device.COMPUTER,
+    livingroomColor: null,
+    bedroomColor: null,
   });
-  const [color, setColor] = useColor("hex", "#121212");
-
-  function updateLights(color: Color) {
-    const hsv = color.hsv;
-    console.log(`sending request with hsv:`);
-    console.dir(hsv);
-    fetch(`http://localhost:3001/color?device=${state.currentDevice}`, {
-      method: 'PUT',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({color: hsv}),
-    }).then((data) => console.log('done'));
-  }
 
   useEffect(() => {
+    console.log('ran useeffect');
     fetch(`http://localhost:3001/color?device=${state.currentDevice}`)
       .then((resp) => resp.json())
       .then((currentColor: ColorResponse) => {
         const {h, s, v} = currentColor;
-        setState({ ...state, initialQueryCompleted: true });
-        setColor(toColor('hsv', {h, s, v}));
+        if (state.currentDevice === Device.BEDROOM) {
+          console.log('setting state: ');
+          console.dir({h, s, v});
+          setState({...state, bedroomColor: toColor('hsv', {h, s, v})});
+        } else {
+          setState( { ...state, livingroomColor: toColor('hsv', {h, s, v})});
+        }
       });
-  }, []);
+  }, [state.currentDevice]);
 
   function setCurrentDevice(newDevice: Device) {
     setState({ ...state, currentDevice: newDevice });
   }
 
 
-  if (state.initialQueryCompleted) {
-    return (
-      <div className="App">
+  if (state.livingroomColor || state.bedroomColor) {
+    if (state.currentDevice === Device.BEDROOM && state.bedroomColor) {
+      return (
+        <div className="App">
           <div>
             <DevicePicker setCurrentDevice={(currentDevice: Device) => setCurrentDevice(currentDevice)} currentDevice={state.currentDevice} />
           </div>
+          <Picker currentDevice={state.currentDevice} initialColor={state.bedroomColor} />
+        </div>
+      )
+    } else if (state.currentDevice === Device.COMPUTER && state.livingroomColor) {
+      return (
+        <div className="App">
           <div>
-            <ColorPicker width={456} color={color} onChange={setColor} onChangeComplete={(color) => updateLights(color)} dark />
+            <DevicePicker setCurrentDevice={(currentDevice: Device) => setCurrentDevice(currentDevice)} currentDevice={state.currentDevice} />
           </div>
+          <Picker currentDevice={state.currentDevice} initialColor={state.livingroomColor} />
+        </div>
+      )
+    }
+    return (
+      <div className="App">
+        <FontAwesomeIcon icon={faSpinner} />
       </div>
-    )
+    );
   }
 
   return (
     <div className="App">
+      <FontAwesomeIcon icon={faSpinner} />
     </div>
   );
 }
---
+++ src/Picker.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { ColorPicker, useColor, toColor, Color } from 'react-color-palette'
+import { Device } from './DevicePicker';
+import {constants} from "os";
+
+interface Props {
+  currentDevice: Device;
+  initialColor: Color;
+}
+
+export default function Picker(props: Props) {
+  const [color, setColor] = useColor('hsv', props.initialColor.hsv);
+
+  function updateLights(color: Color) {
+    const hsv = color.hsv;
+    setColor(color);
+    fetch(`http://localhost:3001/color?device=${props.currentDevice}`, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({color: hsv}),
+    })
+      .then((response) => {
+        if (!response.ok && response.status !== 429) {
+          console.log(`error code: ${response.status}`);
+          console.error(response.statusText);
+        } else {
+          console.log('too many requests');
+        }
+      })
+  }
+  if (props.initialColor) {
+    return (
+      <div className="color-picker">
+        <ColorPicker width={Math.min(456, window.innerWidth * 0.8)} color={color}
+                     onChange={(color) => updateLights(color)}
+        />
+        <div id="preview" style={{backgroundColor: color.hex}}/>
+      </div>
+    );
+  } else {
+    return null;
+  }
+
+}
